3c9bd5a3ef65de38d0e4598bb4e5686759165a4d
sanggatika <sanggatika@gmail.com>
2021-08-24 16:32:45 +0700

    project openmusic v1

---
+++ src/services/postgres/OpenMusicService.js
@@ -0,0 +1,89 @@
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const { mapDBToModel } = require('../../utils');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+// eslint-disable-next-line no-unused-vars
+class OpenMusicService {
+  constructor() {
+    // eslint-disable-next-line no-underscore-dangle
+    this._pool = new Pool();
+  }
+
+  async addSongs({
+    title, year, performer, genre, duration,
+  }) {
+    const id = `song-${nanoid(16)}`;
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const query = {
+      text: 'INSERT INTO m_songs VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
+      values: [id, title, year, performer, genre, duration, insertedAt, updatedAt],
+    };
+
+    // eslint-disable-next-line no-underscore-dangle
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getSongs() {
+    // eslint-disable-next-line no-underscore-dangle
+    const result = await this._pool.query('SELECT id, title, performer FROM m_songs');
+    return result.rows.map(mapDBToModel);
+  }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM m_songs WHERE id = $1',
+      values: [id],
+    };
+    // eslint-disable-next-line no-underscore-dangle
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBToModel)[0];
+  }
+
+  async editSongById(id, {
+    title, year, performer, genre, duration,
+  }) {
+    const updatedAt = new Date().toISOString();
+
+    const query = {
+      text: 'UPDATE m_songs SET title = $1, year = $2, performer = $3, genre = $4, duration = $5,  updated_at = $6 WHERE id = $7 RETURNING id',
+      values: [title, year, performer, genre, duration, updatedAt, id],
+    };
+
+    // eslint-disable-next-line no-underscore-dangle
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
+    }
+  }
+
+  async deleteSongById(id) {
+    const query = {
+      text: 'DELETE FROM m_songs WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    // eslint-disable-next-line no-underscore-dangle
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
+    }
+  }
+}
+
+module.exports = OpenMusicService;
